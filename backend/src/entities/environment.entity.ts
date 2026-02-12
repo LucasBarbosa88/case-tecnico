40df34245e23f3db494f052252deb69c212971d7
Lucas Barbosa <lucas97ti@gmail.com>
2026-02-12 15:17:50 -0300

    created entity environment

---
+++ backend/src/entities/environment.entity.ts
@@ -0,0 +1,51 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { AccessLog } from './access-log.entity';
+
+export enum EnvironmentType {
+  CLASSROOM = 'classroom',
+  LABORATORY = 'laboratory',
+  STUDY_ROOM = 'study_room',
+}
+
+@Entity('environments')
+export class Environment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 255, unique: true })
+  name: string;
+
+  @Column({ type: 'enum', enum: EnvironmentType })
+  type: EnvironmentType;
+
+  @Column({ nullable: true, length: 500 })
+  description: string;
+
+  @Column({ type: 'int' })
+  capacity: number;
+
+  @Column({ nullable: true, length: 100 })
+  building: string;
+
+  @Column({ nullable: true, length: 50 })
+  floor: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @OneToMany(() => AccessLog, (accessLog) => accessLog.environment)
+  accessLogs: AccessLog[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
